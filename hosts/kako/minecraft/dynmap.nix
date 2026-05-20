16c2dc65753adb10e5d6abc4a5cd0e3ef33ae4b4
Blake Haug <blake@blakehaug.com>
2026-05-20 15:10:39 -0700

    change dynmap nginx config

--- hosts/kako/minecraft/dynmap.nix
+++ hosts/kako/minecraft/dynmap.nix
@@ -13,8 +13,13 @@ in
     forceSSL = true;
     locations."/" = {
       proxyPass = "http://127.0.0.1:8123";
-      proxyWebsockets = true;
-      recommendedProxySettings = true;
+      proxyWebsockets = false;
+      extraConfig = ''
+        proxy_set_header Host $host;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_read_timeout 600s;
+      '';
     };
   };
 }
