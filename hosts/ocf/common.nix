4626b9b30fe0966d5d506884194cf1272872da6c
Blake Haug <blake@blakehaug.com>
2026-05-25 17:01:30 -0700

    add ocf server & desktop configs

---
+++ hosts/ocf/common.nix
@@ -0,0 +1,22 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+{
+  home.username = "blakeh";
+  home.homeDirectory = "/home/b/bl/blakeh";
+
+  services.ssh-agent.enable = true;
+
+  programs.ssh.matchBlocks."*".identityFile = lib.mkForce "~/.ssh/id_ed25519_sk.pub";
+
+  programs.git.settings.user.signingkey =
+    lib.mkForce "${config.home.homeDirectory}/.ssh/id_ed25519_sk.pub";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    VISUAL = "nvim";
+  };
+}
