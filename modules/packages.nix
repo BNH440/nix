dcd21627d97a4e7067938825c9e8867d9d99fc85
Blake Haug <blake@blakehaug.com>
2026-07-25 18:30:24 -0700

    enable nix store optimise (#30)

--- modules/packages.nix
+++ modules/packages.nix
@@ -34,6 +34,11 @@
     flake = "/home/blakeh/.nixos";
   };
 
+  nix.optimise = {
+    automatic = true;
+    dates = "weekly";
+  };
+
   programs.tmux.enable = true;
   programs.screen.enable = true;
 
