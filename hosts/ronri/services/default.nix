8f4608306bb877531f52b24fa76137db43cad87e
Blake Haug <blake@blakehaug.com>
2026-09-08 17:06:22 -0700

    simplify stats and make grafana host agnostic

--- hosts/ronri/services/default.nix
+++ hosts/ronri/services/default.nix
@@ -6,6 +6,5 @@
   imports = [
     ./web.nix
     ./cache.nix
-    ./grafana.nix
   ];
 }
--- modules/default.nix
+++ modules/default.nix
@@ -8,7 +8,8 @@
     ./services.nix
     ./system.nix
     ./secrets.nix
-    ./stats.nix
+    ./stats/stats.nix
+    ./stats/grafana.nix
     ./web.nix
     ./pkgs-config.nix
     ./oom.nix
