9f93dac53209e4a48f99449f78a2d306d9cbd8ba
Blake Haug <blake@blakehaug.com>
2026-09-08 18:49:51 -0700

    rework niks3 module for r2 migration

--- hosts/hikari/default.nix
+++ hosts/hikari/default.nix
@@ -143,7 +143,7 @@ in
   nix.settings.trusted-users = [ "blakeh" ];
 
   networking.hostName = "hikari";
-  age.secrets.niks3-auth-token = {
+  age.secrets.niks3-auth-token-client = {
     owner = "blakeh";
     group = "staff";
     rekeyFile = ../../secrets/niks3-auth-token.age;
@@ -151,7 +151,7 @@ in
 
   environment.variables = {
     NIKS3_SERVER_URL = "https://nixcache.blakehaug.com";
-    NIKS3_AUTH_TOKEN_FILE = config.age.secrets.niks3-auth-token.path;
+    NIKS3_AUTH_TOKEN_FILE = config.age.secrets.niks3-auth-token-client.path;
   };
 
   # Set Git commit hash for darwin-version.
